2f3415b33c3ef2e1d2668cf1c5ce35c4943f55c6
Alex Karasik <alexkarasik89@gmail.com>
2017-01-04 15:32:25 -0500

    unit 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,11 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+if(cardOne === cardTwo){
+  alert("You found a match!")
+} else if (cardThree === cardFour){
+  alert("You found a match!")
+} else {
+  alert("Sorry, try again")
+};
